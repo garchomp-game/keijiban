3acb4500f66f544500de362eb50744378f1439c3
garchomp-game <otoka.japanesedrum@gmail.com>
2018-06-27 19:24:49 +0900

    [modify] トップページレイアウト変更

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
     <div class="container">
+        <h2 class='text-center'>掲示板サイトへようこそ！</h2>
         <ul class="list-group original_home_ul_group">
-            <li class="list-group-item"><a class="original_link_button" href="{{route('profile.index')}}">プロフィール</a></li>
             <li class="list-group-item"><a class="original_link_button" href="{{route('boards.index')}}">掲示板一覧へ</a></li>
         </ul>
     </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,27 +60,20 @@
                             <li><a href="{{ route('login') }}">ログイン</a></li>
                             <li><a href="{{ route('register') }}">新規登録</a></li>
                         @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
+                            <li>
+                                <a href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">
+                                ログアウト
+                            </a>
 
-                                <ul class="dropdown-menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                        onclick="event.preventDefault();
-                                        document.getElementById('logout-form').submit();">
-                                        ログアウト
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        {{ csrf_field() }}
-                                    </form>
-                                </li>
-                                <li><a href="{{ route('profile.index') }}">マイページ</a></li>
-                                <li><a href="{{ route('boards.index') }}">掲示板一覧</a></li>
-                            </ul>
-                        </li>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                {{ csrf_field() }}
+                            </form>
+                            </li>
+                            <li><a href="{{ route('profile.show', user('id')) }}">マイページ</a></li>
+                            <li><a href="{{ route('profile.index') }}">ユーザー一覧</a></li>
+                            <li><a href="{{ route('boards.index') }}">掲示板一覧</a></li>
                     @endguest
                 </ul>
             </div>
