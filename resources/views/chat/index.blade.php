1ad88f54eddf4be82b0d5c2af2ac02e33a646df5
garchomp-game <otoka.japanesedrum@gmail.com>
2018-06-30 00:57:34 +0900

    [modify] トップページ、掲示板一覧 掲示板詳細修正

--- resources/views/chat/index.blade.php
+++ resources/views/chat/index.blade.php
@@ -1,62 +1,64 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="col-md-10 col-md-offset-1">
-    {{Form::open(['route' => 'chat.store', 'class' => 'form-group'])}}
-        @if ($errors->first('comment'))
-            <p style="color:red;">{{$errors->first('comment')}}</p>
-        @endif
-        <input type="hidden" name="boards_id" value="{{request()->chat}}">
-        <input type="hidden" name="user_id" value="{{auth()->user()->id}}">
-        <textarea class="form-control" name="comment" rows="6"></textarea>
-        <input class="btn btn-md btn-primary pull-left" style="margin:10px 0" type="submit" name="submit" value="送信">
-        <a href="{{route('boards.index')}}" class="pull-right btn btn-md btn-success" style="margin:10px 0">一覧に戻る</a>
-    {{Form::close()}}
-    <div style="clear:both"></div>
-        <div class="panel panel-default original-chat-bord" id="ul_list">
-            <div class="panel-body">
-                <h2>{{$board->title}}</h2>
-                <hr style="border: black solid 1px">
-                <ul class="list-group">
-                    @if($chats->count())
-                        @foreach($chats as $chat)
-                                <li class="list-group-item original-list-group-item list-group-item-action" id="msg_id{{$chat->id}}">
-                                    {{-- TODO:モーダル表示 --}}
-                                    {{-- <a class="original-chat-link-button" href="#user_modal" rel="modal:open"> --}}
-                                    <a href="{{route('profile.show', $chat->user_id)}}" class="original-chat-link-button">
-                                        <p style="margin:0;">{{$chat->user->name}}</p>
-                                        <hr style="margin:10px 0;">
-                                        <span class=" original-span-with original-comment-style pull-left">{{$chat->comment}}</span>
-                                        {{Form::open(['route' => ['chat.destroy', $chat->id], 'method' => 'delete', 'name' => "destroy{$chat->id}"])}}
-                                            <a class="btn btn-danger pull-right" href="javascript:destroy{{$chat->id}}.submit()">削除</a>
-                                        {{Form::close()}}
-                                        <div style="clear:both"></div>
-                                    </a>
-                                    {{-- </a> --}}
-                                </li>
-                        @endforeach
-                    @else
-                        <li class="list-group-item">まだ投稿がありません</li>
-                    @endif
-                </ul>
-            </div>
+    <div class="container">
+        <div class="col-md-10 col-md-offset-1">
+            <h2 class="pull-left" style="margin:7px 0; font-size:36px; font-weight: bold;">{{$board->title}}</h2>
+            @if ($board->user_id == auth()->user()->id)
+                <a class="btn btn-md pull-right btn-danger" href="javascript:delete.submit();" style="margin:10px 0; margin-left:10px;"><i class="glyphicon glyphicon-trash"></i> 削　除</a>
+                <a class="btn btn-warning pull-right btn-md" style="margin:10px 0;" href="{{route('boards.edit', $board->id)}}"><i class="glyphicon glyphicon-pencil"></i> 編　集</a>
+            @endif
+            <div style="clear:both"></div>
+            <hr style="border:1px solid black; margin:3px 0 15px;">
+            {{Form::open(['route' => ['boards.destroy', $board->id], 'name' => 'delete'])}}
+            {{Form::close()}}
+            {{Form::open(['route' => 'chat.store', 'class' => 'form-group'])}}
+            @if ($errors->first('comment'))
+                <p style="color:red;">{{$errors->first('comment')}}</p>
+            @endif
+            <input type="hidden" name="boards_id" value="{{request()->chat}}">
+            <input type="hidden" name="user_id" value="{{auth()->user()->id}}">
+            <textarea class="form-control" name="comment" rows="6"></textarea>
+            <input class="btn btn-primary pull-left" style="margin:10px 0; font-size:24px; padding:5px 30px;" type="submit" name="submit" value="送信">
+            {{Form::close()}}
+            <div style="clear:both"></div>
+            <ul class="list-group">
+                @if($chats->count())
+                    @foreach($chats as $chat)
+                        <li class="list-group-item original-list-group-item list-group-item-action" id="msg_id{{$chat->id}}">
+                            {{-- TODO:モーダル表示 --}}
+                            {{-- <a class="original-chat-link-button" href="#user_modal" rel="modal:open"> --}}
+                            <a href="{{route('profile.show', $chat->user_id)}}" class="original-chat-link-button">
+                                <p style="margin:0;">{{$chat->user->name}}</p>
+                                <hr style="margin:10px 0;">
+                                <span class=" original-span-with original-comment-style pull-left">{{$chat->comment}}</span>
+                                {{Form::open(['route' => ['chat.destroy', $chat->id], 'method' => 'delete', 'name' => "destroy{$chat->id}"])}}
+                                <a class="btn btn-danger pull-right" href="javascript:destroy{{$chat->id}}.submit()">削除</a>
+                                {{Form::close()}}
+                                <div style="clear:both"></div>
+                            </a>
+                            {{-- </a> --}}
+                        </li>
+                    @endforeach
+                @else
+                    <li class="list-group-item">まだ投稿がありません</li>
+                @endif
+            </ul>
         </div>
     </div>
-</div>
-{{-- TODO:モーダル表示 --}}
-{{-- <div id="user_modal" class="modal">
-  <p></p>
-  <a href="#" rel="modal:close">Close</a>
+    {{-- TODO:モーダル表示 --}}
+    {{-- <div id="user_modal" class="modal">
+    <p></p>
+    <a href="#" rel="modal:close">Close</a>
 </div> --}}
 
 <script type="text/javascript">
-    $(function() {
-        $("#msg_id{{$chats->count()}}").click;
-    })
+$(function() {
+    $("#msg_id{{$chats->count()}}").click;
+})
 </script>
 @endsection
 {{--
 TODO:jqueryを使ったスクロール問題未解決。各liにidだけ割当
 うまくスクロールできないのでもし解決法わかったらお願いしますm(_ _)m
- --}}
+--}}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,32 +7,25 @@
             {{Form::open(['route' => 'boards.index', 'method' => 'get', 'class' => 'form-group'])}}
             <!-- Search form -->
             <div class="md-form" style="margin-bottom:10px;">
-                <input class="form-control" type="text" name="title" placeholder="掲示板タイトル" aria-label="掲示板タイトル">
+                <div class="col-md-7"></div>
+                <input class="col-md-4" type="text" name="search_box" placeholder="掲示板タイトル" aria-label="掲示板タイトル">
             </div>
-            <div class="md-form" style="margin-bottom:10px;">
-                <input class="form-control" type="text" name="description" placeholder="掲示板詳細" aria-label="掲示板検索">
-            </div>
-            <input class="form-control btn-success" type="submit" name="submit" value="検索">
+            <button type="submit" name="submit" class="col-md-1 btn-success"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
             {{Form::close()}}
+            <div style="clear:both;margin-bottom:30px">
+
+            </div>
+            <h3>新着部屋一覧</h3>
             @if($boards->count())
                 <ul class="list-group">
                     @foreach($boards as $board)
-                        <li class="list-group-item">
+                        <a class="list-group-item" href="{{route('boards.show', $board->id)}};">
                             <h3 class="pull-left" style="display:block; margin:0 0 10px;">{{$board->title}}</h3>
                             <div style="clear:both"></div>
                             <span class="pull-left original-span-with" style="float:left;">{{$board->description}}</span><br>
                             <div style="clear:both"></div>
-                            <span class="pull-right">
-                                <a class="btn btn-md btn-success original-button" href="{{ route('boards.show', $board->id) }}"><i class="glyphicon glyphicon-list"></i> 掲示板詳細</i></a>
-                                @if ($board->user_id == auth()->user()->id)
-                                    {{Form::open(['route' => ['boards.destroy', $board->id], 'method' => 'delete', 'class' => 'form_inline', 'onsubmit' => "return confirm('本当に消しますか？');"])}}
-                                    <button type="submit" class="btn btn-md  btn-danger original-button"><i class="glyphicon glyphicon-trash"></i> 削　除</button>
-                                    {{Form::close()}}
-                                    <a class="btn btn-warning btn-md original-button" href="{{route('boards.edit', $board->id)}}"><i class="glyphicon glyphicon-pencil"></i> 編　集</a>
-                                @endif
-                            </span>
                             <div style="clear:both"></div>
-                        </li>
+                        </a>
                     @endforeach
                 </ul>
                 {!! $boards->render() !!}
