1e2430a58a6147ecd4bafec3624705e086cca69f
garchomp-game <otoka.japanesedrum@gmail.com>
2018-07-02 00:32:33 +0900

    [mdoify]ファボ表示調整

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -3,12 +3,15 @@
 @section('content')
     <div class="container">
         <ul class='list-group'>
+            <h2>ユーザー一覧</h2>
             @foreach ($profile as $key => $value)
                 <a href="{{route('profile.show', $value->id)}}" class='list-group-item row original-margin-15-0'>
                     <h3 class='col-md-4'>{{$value->name}}</h3>
                     <span class="col-md-7">{{$value->description}}</span>
-                    <span href="{{route('follow', $value->id)}}" class="btn col-md-1"
-                        style="font-size:36px;">💖</span>
+                    @if (user('id') != $value->id)
+                        <span href="{{route('follow', $value->id)}}" class="btn col-md-1"
+                            style="font-size:36px;">💖</span>
+                    @endif
                 </a>
             @endforeach
         </ul>
